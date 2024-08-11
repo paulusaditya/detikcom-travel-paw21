5671b214ab03f94be0ff161258734809c4d5adc9
Paulus <222410103052@unej.ac.id>
2024-08-12 02:37:54 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,79 @@
+function previewImage(event) {
+    const input = event.target;
+    const reader = new FileReader();
+    const preview = document.getElementById('preview');
+    const icon = document.querySelector('.file-upload-wrapper .icon');
+
+    // Cek apakah file yang dipilih adalah gambar
+    if (input.files && input.files[0]) {
+        reader.onload = function(e) {
+            preview.src = e.target.result; // Menampilkan gambar
+            preview.style.display = 'block'; // Menampilkan elemen img
+            icon.style.display = 'none'; // Menyembunyikan ikon "+"
+        }
+
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
+var nextBtn = document.querySelector('.next'),
+    prevBtn = document.querySelector('.prev'),
+    carousel = document.querySelector('.carousel'),
+    list = document.querySelector('.list'), 
+    item = document.querySelectorAll('.item'),
+    runningTime = document.querySelector('.carousel .timeRunning') 
+
+let timeRunning = 3000 
+let timeAutoNext = 7000
+
+nextBtn.onclick = function(){
+    showSlider('next')
+}
+
+prevBtn.onclick = function(){
+    showSlider('prev')
+}
+
+let runTimeOut 
+
+let runNextAuto = setTimeout(() => {
+    nextBtn.click()
+}, timeAutoNext)
+
+
+function resetTimeAnimation() {
+    runningTime.style.animation = 'none'
+    runningTime.offsetHeight /* trigger reflow */
+    runningTime.style.animation = null 
+    runningTime.style.animation = 'runningTime 7s linear 1 forwards'
+}
+
+
+function showSlider(type) {
+    let sliderItemsDom = list.querySelectorAll('.carousel .list .item')
+    if(type === 'next'){
+        list.appendChild(sliderItemsDom[0])
+        carousel.classList.add('next')
+    } else{
+        list.prepend(sliderItemsDom[sliderItemsDom.length - 1])
+        carousel.classList.add('prev')
+    }
+
+    clearTimeout(runTimeOut)
+
+    runTimeOut = setTimeout( () => {
+        carousel.classList.remove('next')
+        carousel.classList.remove('prev')
+    }, timeRunning)
+
+
+    clearTimeout(runNextAuto)
+    runNextAuto = setTimeout(() => {
+        nextBtn.click()
+    }, timeAutoNext)
+
+    resetTimeAnimation() // Reset the running time animation
+}
+
+// Start the initial animation 
+resetTimeAnimation()
